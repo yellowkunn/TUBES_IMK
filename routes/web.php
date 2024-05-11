1235793194b44a1ea6034983b236a24c6851947d
yellowkunn <ahsanlubis308@gmail.com>
2024-05-11 18:07:38 +0700

    bismillah anjay(aman jaya)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -37,5 +37,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// OWNER
+Route::get('/admindashboard', [AdminController::class, 'dashboardadmin']);
 
 require __DIR__.'/auth.php';
