011f7ef1b3e000b34df5ce48b480638d52ffdf6b
Yohana Septamia <yohanaseptamia@gmail.com>
2024-05-19 00:35:44 +0700

    responsif nav and footer user

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,37 +1,64 @@
-<footer class="flex flex-col gap-6 p-12 px-24 text-white w-full" style="background-color: #485C7D;">
-    <div class="flex justify-center gap-4">
-        <a href="" class="bg-white text-black p-1 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 2h-3a5 5 0 0 0-5 5v3H6v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
-        </a>
-    
-        <a href="" class="bg-white text-black p-1 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg>    
-        </a>
+<footer>
+    <div class="md:grid grid-cols-2">
+    <div class="flex flex-col gap-6 p-12 text-white" style="background-color: #415474;">
+        <p class="text-white font-semibold text-subtitle leading-snug">Fortune<br>Education Center</p>
+
+        <div class="flex flex-col gap-2">
+            <a href="">Tentang Kami</a>
+            <a href="">FAQ</a>
+            <a href="">S&K</a>
+            <a href="">Kebijakan Hak Cipta</a>
+        </div>
+
+        <hr>
+        <div class="md:flex justify-between items-center">
+            <p class="hidden md:block text-smallContent">© 2024 All Rights Reserved</p>
+
+            <div class="flex justify-start md:justify-center gap-4">
+                <button id="fb" class="bg-white text-black p-1 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 2h-3a5 5 0 0 0-5 5v3H6v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
+                </button>
+            
+                <button id="ig" class="bg-white text-black p-1 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg>    
+                </button>
+
+                <button id="phone" class="bg-white text-black p-1 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24"><path fill="currentColor" d="M19.44 13c-.22 0-.45-.07-.67-.12a9.44 9.44 0 0 1-1.31-.39a2 2 0 0 0-2.48 1l-.22.45a12.18 12.18 0 0 1-2.66-2a12.18 12.18 0 0 1-2-2.66l.42-.28a2 2 0 0 0 1-2.48a10.33 10.33 0 0 1-.39-1.31c-.05-.22-.09-.45-.12-.68a3 3 0 0 0-3-2.49h-3a3 3 0 0 0-3 3.41a19 19 0 0 0 16.52 16.46h.38a3 3 0 0 0 2-.76a3 3 0 0 0 1-2.25v-3a3 3 0 0 0-2.47-2.9m.5 6a1 1 0 0 1-.34.75a1.05 1.05 0 0 1-.82.25A17 17 0 0 1 4.07 5.22a1.09 1.09 0 0 1 .25-.82a1 1 0 0 1 .75-.34h3a1 1 0 0 1 1 .79q.06.41.15.81a11.12 11.12 0 0 0 .46 1.55l-1.4.65a1 1 0 0 0-.49 1.33a14.49 14.49 0 0 0 7 7a1 1 0 0 0 .76 0a1 1 0 0 0 .57-.52l.62-1.4a13.69 13.69 0 0 0 1.58.46q.4.09.81.15a1 1 0 0 1 .79 1Z"/></svg>
+                </button>
+
+                <button id="email" class="bg-white text-black p-1 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 1024 1024"><path fill="currentColor" d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32m-40 110.8V792H136V270.8l-27.6-21.5l39.3-50.5l42.8 33.3h643.1l42.8-33.3l39.3 50.5zM833.6 232L512 482L190.4 232l-42.8-33.3l-39.3 50.5l27.6 21.5l341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5l-39.3-50.5z"/></svg>
+                </button>
+            </div>
+
+            <p class="block md:hidden text-smallContent mt-6">© 2024 All Rights Reserved</p>
+        </div>
     </div>
 
-    <div class="flex justify-center gap-12">
-        <a href="">Tentang Kami</a>
-        <a href="">Promo</a>
-        <a href="">Daftar</a>
-        <a href="">Kebijakan Hak Cipta</a>
+    <div class="w-full p-12" style="background-color: #415474;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3603.587295327313!2d98.70254632803451!3d3.5659308713442055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30313058c7999413%3A0x31b26aea0291234f!2sFortunate%20Education%20Center!5e1!3m2!1sid!2sid!4v1716044761765!5m2!1sid!2sid" 
+            class="w-full h-full border-none" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        
     </div>
-    
-    <div class="flex justify-center gap-4 mt-12">
-        <a href="" class="flex justify-center gap-2 items-center bg-white rounded-full p-2 px-4" style="color: #485C7D;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M12.56 20.82a.964.964 0 0 1-1.12 0C6.611 17.378 1.486 10.298 6.667 5.182A7.592 7.592 0 0 1 12 3c2 0 3.919.785 5.333 2.181c5.181 5.116.056 12.196-4.773 15.64"/><path d="M12 12a2 2 0 1 0 0-4a2 2 0 0 0 0 4"/></g></svg>
-            <p>lokasi</p>
-        </a>
-
-        <a href="" class="flex justify-center gap-2 items-center bg-white rounded-full p-2 px-4" style="color: #485C7D;">
-            <i class="fa-solid fa-phone"></i>
-            <p>+1234567</p>
-        </a>
-        <a href="" class="flex justify-center gap-2 items-center bg-white rounded-full p-2 px-4" style="color: #485C7D;">
-            <i class="fa-solid fa-envelope fa-md"></i>
-            <p>fortune@gmail.com</p>
-        </a>
     </div>
-
-    <hr class="text-white">
-    <p class="text-smallContent text-center">© 2024 All Rights Reserved</p>
 </footer>
+
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/tippy.js@6"></script>
+<script>
+    tippy('#fb', {
+    content: 'Fortune Education Center',
+    });
+
+    tippy('#ig', {
+    content: '@fortune_education_center',
+    });
+
+    tippy('#phone', {
+    content: '+12345678',
+    });
+
+    tippy('#email', {
+    content: 'fortuneec@gmail.edu',
+    });
+</script>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
-<div class="mt-2 bg-white">
-<div class="flex justify-between items-center px-12 py-5">
+<div class="mt-2 bg-white hidden md:block text-smallContent">
+<div class="flex justify-between items-center px-8 lg:px-12 py-5">
 
     <div class="flex gap-8 lg:gap-24">
-    <p class="text-baseBlue text-center font-bold text-[18px] leading-snug">Fortunate<br>Education Center</p>
-    <div id="menus" class="flex gap-12 items-center">
+    <p class="text-baseBlue text-center font-bold lg:text-[18px] leading-snug">Fortunate<br>Education Center</p>
+    <div id="menus" class="flex gap-5 lg:gap-12 items-center">
         <a href="" class="nav">Beranda</a>
         <a href="" class="nav">Kelas</a>
         <a href="" class="nav">Langkah Pendaftaran</a>
@@ -16,3 +16,57 @@
     </div>
 </div>
 </div>
+
+<div class="relative block md:hidden">
+    <div class="flex justify-between items-center px-6 md:px-8 py-5">
+        <p class="text-baseBlue text-center font-bold leading-snug">Fortunate<br>Education Center</p>
+
+        <div class="flex items-center gap-2">
+            <a href="{{ route('login') }}" id="masuk"><i class="fa-solid fa-user text-greyIcon"></i></a>
+
+            <button id="hamburger" type="button" class="p-2 px-3 rounded-xl hover:bg-neutral-100">
+                <i class="fa-solid fa-bars fa-lg text-greyIcon"></i>
+            </button>
+        </div>
+    </div>
+
+    <div class="hidden md:hidden z-10 absolute top-0" id="menuHamburger">
+        <div class="bg-white w-screen flex flex-col gap-5 p-10">
+        <!-- close button -->
+        <div class="flex justify-end">
+            <i class="fa-solid fa-xmark fa-lg text-slate-600" id="btnClose"></i>
+        </div>
+
+        <div id="menus" class="flex flex-col gap-6">
+            <a href="" class="nav w-fit">Beranda</a>
+            <a href="" class="nav w-fit">Kelas</a>
+            <a href="" class="nav w-fit">Langkah Pendaftaran</a>
+
+            <a href="{{ route('login') }}">Masuk/Daftar</i></a>
+        </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/tippy.js@6"></script>
+<script>
+    tippy('#masuk', {
+    content: 'Masuk/Daftar',
+    });
+
+
+    const hamburger = document.getElementById('hamburger');
+    const menuHamburger = document.getElementById('menuHamburger');
+    const btnClose = document.getElementById('btnClose');
+
+    hamburger.addEventListener('click', () => {
+        menuHamburger.classList.remove('hidden');
+        hamburger.classList.add('hidden');
+    });
+
+    btnClose.addEventListener('click', () => {
+        menuHamburger.classList.add('hidden');
+        hamburger.classList.remove('hidden');
+    })
+</script>
