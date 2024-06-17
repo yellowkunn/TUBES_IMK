b7b6f1e08234e053f6b3c1cea53508d1b35fe39f
Yohana Septamia <yohanaseptamia@gmail.com>
2024-06-17 12:09:57 +0700

    tes

---
+++ resources/views/user/notifikasi.blade.php
@@ -0,0 +1,34 @@
+@include('htmlhead')
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notifikasi</title>
+
+    <!-- google font for icon -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <!-- font awsome -->
+    <script src="https://kit.fontawesome.com/8c8655eff1.js" crossorigin="anonymous"></script>
+
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="font-Inter dark:dark-mode">
+    @include('components.navbar')
+
+    <div id="content" class="px-12 md:px-24 py-10 dark:border-t-2 dark:border-white">
+
+       
+    </div>
+
+@include('components.footer')
+
+</body>
+
+</html>
