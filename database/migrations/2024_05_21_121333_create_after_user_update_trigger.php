93eab6726e22cfda33313accc8de482b2018ea48
Yohana Septamia <yohanaseptamia@gmail.com>
2024-06-18 18:59:03 +0700

    pindahkan gambar

--- database/migrations/2024_05_21_121333_create_after_user_update_trigger.php
+++ database/migrations/2024_05_21_121333_create_after_user_update_trigger.php
@@ -14,22 +14,22 @@ return new class extends Migration
      */
     public function up()
     {
-        DB::unprepared('
-        CREATE TRIGGER after_user_insert
-        AFTER INSERT ON users
-        FOR EACH ROW
-        BEGIN
-            -- Hanya lakukan sesuatu jika peran pengguna adalah "siswa"
-            IF NEW.role = "siswa" THEN
-                -- Periksa apakah user_id sudah ada di tabel siswa
-                IF NOT EXISTS (SELECT 1 FROM siswa WHERE pengguna_id = NEW.id_pengguna) THEN
-                    -- Jika belum ada, masukkan data baru ke tabel siswa
-                    INSERT INTO siswa (pengguna_id)
-                    VALUES (NEW.id_pengguna);
-                END IF;
-            END IF;
-        END
-    ');
+    //     DB::unprepared('
+    //     CREATE TRIGGER after_user_insert
+    //     AFTER INSERT ON users
+    //     FOR EACH ROW
+    //     BEGIN
+    //         -- Hanya lakukan sesuatu jika peran pengguna adalah "siswa"
+    //         IF NEW.role = "siswa" THEN
+    //             -- Periksa apakah user_id sudah ada di tabel siswa
+    //             IF NOT EXISTS (SELECT 1 FROM siswa WHERE pengguna_id = NEW.id_pengguna) THEN
+    //                 -- Jika belum ada, masukkan data baru ke tabel siswa
+    //                 INSERT INTO siswa (pengguna_id)
+    //                 VALUES (NEW.id_pengguna);
+    //             END IF;
+    //         END IF;
+    //     END
+    // ');
 
     DB::unprepared('
     CREATE TRIGGER after_user_update
